ec67062bbd88484c1510e81117a9e0d71722772d
YuanSamuel <samuel6161@gmail.com>
2022-08-16 12:55:19 -0500

    log

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from 'next/head';
 import { useSession } from 'next-auth/react';
 import LoginButton from '../components/login-button';
+ 
+console.log(process.env.FACEBOOK_CLIENT_ID)
 
 export default function Home() {
   const { data: session, status } = useSession();
   const loading = status === 'loading';
-  console.log(process.env.FACEBOOK_CLIENT_ID)
+  
   return (
     <div>
       <Head>
@@ -13,7 +15,6 @@ export default function Home() {
         <meta name='description' content='DNBA Giveaway Manager' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-
       <LoginButton />
     </div>
   );
